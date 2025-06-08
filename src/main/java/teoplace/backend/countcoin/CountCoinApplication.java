c81893da8e2d89b45470987c3af746c17d8b2d53
TeoDublin <teodublin@gmail.com>
2025-06-08 17:28:19 +0200

    first commit

---
+++ src/main/java/teoplace/backend/countcoin/CountCoinApplication.java
@@ -0,0 +1,13 @@
+package teoplace.backend.countcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountCoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountCoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teoplace/backend/countcoin/CountCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package teoplace.backend.countcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountCoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
